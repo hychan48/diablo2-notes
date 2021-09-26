25676dbf6678e61dfd8e9d4e7af6b6f0071f4f15
JC <jchan48@gmail.com>
2021-09-26 04:00:39 -0400

    added mocha.. but config might be wrong

--- test/convertToContent.test.js
+++ test/convertToContent.test.js
@@ -30,20 +30,49 @@ this.timeout(500);//500ms
  */
 
 export const weapons = [
-
+'Axes',
+'Bows',
+'Crossbows',
+'Daggers',
+'Javelins',
+'Maces',
+'Polearms',
+'Scepters',
+'Spears',
+'Staves',
+'Swords',
+'Throwing',
+'Wands'
 ];
 export const classWeapons = [
-
+// • Ama Weapons
+'Claws',
+// Helms,//barb
+// • Druid Helms
+// • Necro Totems, //heads
+// • Pal Shields
+// 'orbs',• Sorc Orbs
 ];
 
-export const shields = [];
+export const shields = [];//should be straight forward?
 
-export const chestArmors = [];
-export const helmArmors = [];
+export const chestArmors = [];//chest?
+export const helmArmors = [];//helm / head? it shouldnt matter i guess?
 
+const XLSX = require('xlsx');
+function readXLSX(filepath='~/static/rawFiles/socketItems.xlsx'){
+  const wb = XLSX.readFile(filepath);
+  const ws_name = "sheet1";
+  const ws = wb.Sheets[ws_name];
+  const rows = XLSX.utils.sheet_to_json(ws);
+}
 describe('Convert format to nuxt/$content', function(){
 
-  it('asdf', function(){
+  it('Read xlsx file first', async function(){
     //assert.strictEqual(1,1);//require assert
+    // const oData = await readXLSX();
+    // console.log(oData);
+
+
   });
 });
