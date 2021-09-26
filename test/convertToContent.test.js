4d762423415ed9d8a35c9728f36c4927cba7b0b7
JC <jchan48@gmail.com>
2021-09-26 03:52:30 -0400

    added mocha dependancies

--- test/convertToContent.test.js
+++ test/convertToContent.test.js
@@ -19,9 +19,30 @@ this.timeout(500);//500ms
 
 
 /**
+ * SocketItem.xlsx - append weapon / armor type to the file
+ *
+ * might want to find the unmappable ones first..
+ *
+ * https://diablo2.diablowiki.net/Sockets
+ *
+ * only chest, helm, shield and weapons
  *
  */
+
+export const weapons = [
+
+];
+export const classWeapons = [
+
+];
+
+export const shields = [];
+
+export const chestArmors = [];
+export const helmArmors = [];
+
 describe('Convert format to nuxt/$content', function(){
+
   it('asdf', function(){
     //assert.strictEqual(1,1);//require assert
   });
